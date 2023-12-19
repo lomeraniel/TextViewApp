e34a2852a8fb664e3a036a8027e649516097167b
Elena Dosil <39484218+lomeraniel@users.noreply.github.com>
2023-12-19 13:08:53 +0100

    Fix hardcoded text and color, and modify TextView

--- app/src/main/java/com/example/textviewapp/MainActivity.java
+++ app/src/main/java/com/example/textviewapp/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
+        helloTextView.setText(R.string.user_greeting);
     }
 }
